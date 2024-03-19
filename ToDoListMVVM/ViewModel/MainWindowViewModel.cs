44c5dc2bb063a8280b9de753a398af850582c68c
voxox4450 <voxoxgaming@gmail.com>
2024-03-19 13:56:00 +0100

    how do pinguin?

--- ToDoListMVVM/ViewModel/MainWindowViewModel.cs
+++ ToDoListMVVM/ViewModel/MainWindowViewModel.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows;
-using ToDoListMVVM.Models;
-using ToDoListMVVM.Views;
-using Serilog;
-using System.Collections.ObjectModel;
+﻿using System.Windows;
 using System.Windows.Input;
-using ToDoListMVVM;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using GalaSoft.MvvmLight.Messaging;
 
 namespace ToDoListMVVM.ViewModel
 {
@@ -34,7 +24,6 @@ namespace ToDoListMVVM.ViewModel
 
         private void NextPage()
         {
-            //MessengerInstance.Send(new OpenDialogMessage(typeof(UserControlAddViewModel), "Dodaj notatkę"));
         }
     }
 }
