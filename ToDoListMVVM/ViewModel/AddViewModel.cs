453175e9a3d05e01c8ccd03a9de8f1311f48c34a
voxox4450 <voxoxgaming@gmail.com>
2024-04-05 14:34:47 +0200

    walidacja

--- ToDoListMVVM/ViewModel/AddViewModel.cs
+++ ToDoListMVVM/ViewModel/AddViewModel.cs
@@ -4,10 +4,12 @@ using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
 using ToDoListMVVM.Interface;
 using ToDoListMVVM.Entities;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoListMVVM.ViewModel
 {
-    public class AddViewModel
+    public class AddViewModel : ObservableValidator
     {
         private readonly INoteService _noteService;
         private readonly IDialogService _dialogService;
@@ -33,31 +35,84 @@ namespace ToDoListMVVM.ViewModel
         public List<Priority> Priorities { get; set; }
 
         public List<Status> Statuses { get; set; }
-        public int SelectedPriorities { get; set; }
-        public int SelectedStatuses { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public string TextNote { get; set; }
+        private int _selectedPriorities;
+        private int _selectedStatuses;
+        private DateTime _startDate = DateTime.Today;
+        private DateTime _endDate = DateTime.Today;
+        private string _textNote;
+
+        public int SelectedPriorities
+        {
+            get => _selectedPriorities;
+            set => SetProperty(ref _selectedPriorities, value);
+        }
+
+        public int SelectedStatuses
+        {
+            get => _selectedStatuses;
+            set => SetProperty(ref _selectedStatuses, value);
+        }
+
+        [Required(ErrorMessage = "Proszę podać datę rozpoczęcia.")]
+        public DateTime StartDate
+        {
+            get => _startDate;
+            set => SetProperty(ref _startDate, value);
+        }
+
+        [Required(ErrorMessage = "Proszę podać datę zakończenia.")]
+        [CustomValidation(typeof(EditViewModel), nameof(ValidateEndDate))]
+        public DateTime EndDate
+        {
+            get => _endDate;
+            set => SetProperty(ref _endDate, value);
+        }
+
+        [Required(ErrorMessage = "Pole tekstowe jest wymagane.")]
+        [MaxLength(150, ErrorMessage = "Pole tekstowe nie może zawierać więcej niż 150 znaków.")]
+        public string TextNote
+        {
+            get => _textNote;
+            set => SetProperty(ref _textNote, value);
+        }
+
+        private bool IsValid()
+
+        {
+            ValidateAllProperties();
+
+            return !HasErrors;
+        }
+
+        public static ValidationResult ValidateEndDate(DateTime endDate, ValidationContext context)
+
+        {
+            if (context.ObjectInstance is EditViewModel editViewModel && editViewModel.StartDate > endDate)
+
+            {
+                return new ValidationResult("Wprowadzono błędny zakres dat!");
+            }
+
+            return ValidationResult.Success!;
+        }
 
         private void Add()
         {
-            if (StartDate > EndDate)
+            if (HasErrors)
+
             {
-                MessageBox.Show("Błąd: Data rozpoczęcia musi być mniejsza niż data zakończenia.");
+                return;
             }
-            else
+            var newNote = new Note()
             {
-                var newNote = new Note()
-                {
-                    ContentText = TextNote,
-                    StartDate = StartDate,
-                    EndDate = EndDate,
-                    PriorityId = SelectedPriorities,
-                    StatusId = SelectedStatuses
-                };
-                _noteService.Add(newNote);
-                _dialogService.CloseDialog();
-            }
+                ContentText = TextNote,
+                StartDate = StartDate,
+                EndDate = EndDate,
+                PriorityId = SelectedPriorities,
+                StatusId = SelectedStatuses
+            };
+            _noteService.Add(newNote);
+            _dialogService.CloseDialog();
         }
     }
 }
